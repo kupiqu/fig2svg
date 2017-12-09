f52fd764472f6f6e9ef58c5152de52109c6bd18a
Salva Ardid <jsardid@gmail.com>
2017-12-09 18:33:25 -0500

    added more clipping options (no clipping, box off/on dependent clipping [default], strict axes dependent clipping, axes + data dependent clipping)

--- example/test_legend_and_clipping.m
+++ example/test_legend_and_clipping.m
@@ -9,12 +9,24 @@ fontSize = 16*resolutionScaling;
 lineWidth = 2*resolutionScaling;
 markerSize = 10*resolutionScaling;
 
+figure
 hold on
+set(gca,'Layer','top')
 p = plot(1:10,[1:10;10:-1:1],'s-','LineWidth',lineWidth,'MarkerSize',markerSize);
-set(gca,'Fontsize',fontSize,'LineWidth',lineWidth)
 l = legend(p,{'data1','data2'},'Location','NorthEastOutside','Orientation','Vertical','Fontsize',fontSize,'Box','on','LineWidth',lineWidth);
 % l = legend(p,{'data1','data2'},'Location','SouthWestOutside','Orientation','Vertical','Fontsize',fontSize,'Box','on','LineWidth',lineWidth);
-plot2svg('legend.svg')
+xlim([1,10])
+ylim([1,10])
+% set(gca,'fontSize',fontSize,'lineWidth',lineWidth,'Box','off')
+set(gca,'fontSize',fontSize,'lineWidth',lineWidth,'Box','on')
+xlabel('X','fontSize',fontSize)
+ylabel('Y','fontSize',fontSize)
+
+% plot2svg('legend.svg','','','',0) % no clipping
+plot2svg('legend.svg','','','',1) % box off/on dependent clipping [default]
+% plot2svg('legend.svg','','','',2) % strict axes dependent clipping
+% plot2svg('legend.svg','','','',3) % axes + data dependent clipping
+
 saveas(gca,'legend_saveas.svg')
 
 set(0,'defaultAxesFontName',prevFontName);
