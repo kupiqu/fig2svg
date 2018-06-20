6e5c60558567c5e1afd64a95af5058b5a48517b1
Salva Ardid <jsardid@gmail.com>
2018-06-20 13:54:01 -0400

    version update

--- src/plot2svg.m
+++ src/plot2svg.m
@@ -26,7 +26,7 @@ function varargout = plot2svg(filename, id, debug, legendIcons, clippingMode, fi
 
   global PLOT2SVG_globals
   global colorname
-  progversion = '19-Jun-2018';
+  progversion = '20-Jun-2018';
   PLOT2SVG_globals.runningIdNumber = 0;
   PLOT2SVG_globals.UI = reportUI;
   PLOT2SVG_globals.octave = false;
