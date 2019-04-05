4cb8b18a40bd5a3eaf5f58c0096119591069e759
Salva Ardid <jsardid@gmail.com>
2019-04-05 17:54:58 -0400

    version update

--- src/fig2svg.m
+++ src/fig2svg.m
@@ -26,7 +26,7 @@ function varargout = fig2svg(filename, id, debug, legendIcons, clippingMode, fig
 
   global FIG2SVG_globals
   global colorname
-  release_version = '2019.03.0'; % year.month.incremental
+  release_version = '2019.04.0'; % year.month.incremental
   FIG2SVG_globals.runningIdNumber = 0;
   FIG2SVG_globals.UI = reportUI;
   FIG2SVG_globals.octave = false;
